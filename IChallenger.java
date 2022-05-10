877279c9590b4fcf2458378fc6fcb5cdd3ff4d42
STEVEN KERAUTRET <steven928@hotmail.fr>
2022-05-10 10:17:44 +0200

    add comments

--- IChallenger.java
+++ IChallenger.java
@@ -1,8 +1,21 @@
 import java.util.Set;
 
 public interface IChallenger{
+    
+
+// −−−−−−−−−−−−−−−−−−−−−− Fonctions pour le tournoi −−−−−−−−−−−−−−−−−−−−−
+
+    /*
+    * L ’ arbitre vous demande le nom de votre équipe.
+    * @return le nom de votre équipe sous la forme "Nom1 − Nom2"
+    */
     String teamName();
 
+    /**
+    * L ’ arbitre vous signale votre rôle au début de la partie.
+    * Vous pouvez préparer votre représentation interne du plateau à ce moment.
+    * @param role le rôle qui vous est assigné ( "RED" ou "BLACK" )
+    */
     void setRole(String role);
 
     void iPlay(String move);
