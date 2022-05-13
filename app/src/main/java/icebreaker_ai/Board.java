9db53df7c585a6ada590fff78e7c767957f2e86c
dam <damien.chancerel@gmail.com>
2022-05-13 10:53:37 +0200

    jar

--- app/src/main/java/icebreaker_ai/Board.java
+++ app/src/main/java/icebreaker_ai/Board.java
@@ -3,7 +3,6 @@ package icebreaker_ai;
 public class Board {
     public static void main(String[] args) {
         MyChallenger challenger = new MyChallenger();
-        challenger.setBoardFromFile("Plateau_initial.txt");
 
         challenger.setRole("RED");
         while(!challenger.isOver()){
